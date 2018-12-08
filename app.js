0112a839d8bec44b3a048c39dc2ed0bbf0803770
Sean Brennan <spb4578@gmail.com>
2018-12-08 13:14:37 -0600

    working front end

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
@@ -12,12 +11,12 @@ const app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
@@ -28,7 +27,7 @@ app.use(function(req, res, next) {
 });
 
 let hbs = require("hbs");
-hbs.registerPartials(__dirname + "views/partials");
+hbs.registerPartials(__dirname + "views");
 // error handler
 app.use(function(err, req, res) {
     // set locals, only providing error in development
