9e9b038a6ebf266248dffd349d457df6f2780f2a
mattspoche <matthewspoche@comcast.net>
2018-12-11 17:58:47 -0600

    codemirror in the search results

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // post modal functions
-    $("#upload").on("click", function(event) {
+    $("#upload").on("click", function (event) {
         event.preventDefault();
         console.log("submitCode function ran");
         const data = {
@@ -38,14 +38,14 @@ $(document).ready(function() {
             }).then(() => {
                 console.log("sent data");
                 $("#successMsg").html("<div class='loader'></div>");
-                setTimeout(function() {
+                setTimeout(function () {
                     $("#upload").addClass("success");
                     $("#upload").html("posted");
                     $("#successMsg").html("success, your code has been added");
                     $("#successMsg").css("color", "green");
                 }, 4000);
 
-                setTimeout(function() {
+                setTimeout(function () {
                     $("#upload").removeClass("success");
                     $("#upload").html("post");
                     $("#successMsg").html("");
@@ -63,7 +63,7 @@ $(document).ready(function() {
     });
 
     //when user changes #laguage dropdown value, update codemirror
-    $("#language").change(function() {
+    $("#language").change(function () {
         console.log("language changed: ", $("#language").val());
         let lang = $("#language").val();
         if (lang === "html") {
@@ -77,25 +77,48 @@ $(document).ready(function() {
     });
 
     //code mirror initialize
-    let myTextArea = document.getElementById("codesnip");
-    let code = myTextArea.innerText;
+    let myCodeMirror;
+    if (document.getElementById("codesnip")) {
 
-    console.log(myTextArea);
-    console.log(code);
+        let myTextArea = document.getElementById("codesnip");
 
-    let myCodeMirror = CodeMirror(
-        function(elt) {
-            myTextArea.parentNode.replaceChild(elt, myTextArea);
-        },
-        {
-            value: code,
-            mode: "javascript",
-            theme: "3024-night"
-        }
-    );
+        let code = myTextArea.innerText;
+
+        console.log(myTextArea);
+        console.log(code);
+
+        myCodeMirror = CodeMirror(
+            function (elt) {
+                myTextArea.parentNode.replaceChild(elt, myTextArea);
+            },
+            {
+                value: code,
+                mode: "javascript",
+                theme: "3024-night"
+            }
+        );
+    }
+    //console.log("here");
+    let codelist = [];
+    $(".search-code").each(function (index) {
+        //console.log("is this shit fucking working?????????????");
+        console.log($(this).text());
+        let textArea = this;
+        let text = textArea.innerText;
+        codelist.push(CodeMirror(
+            function(elt) {
+                textArea.parentNode.replaceChild(elt, textArea);
+            },
+            {
+                value: text,
+                mode: "javascript",
+                theme: "3024-night"
+            }
+        ))        
+    });
     //end code mirror
 
-    $("#searchSubmit").on("click", function(event) {
+    $("#searchSubmit").on("click", function (event) {
         event.preventDefault();
         let keywords = $("#selectedKeyword")
             .val()
@@ -108,11 +131,11 @@ $(document).ready(function() {
             $.ajax({
                 url: `/api/keywords/${keywords}`,
                 method: "GET"
-            }).then(function(result) {
+            }).then(function (result) {
                 console.log(result);
             });
             $("#selectedKeyword").val();
-            setTimeout(function() {
+            setTimeout(function () {
                 location.assign(
                     `http://localhost:8080/api/keywords/${keywords}`
                 );
@@ -121,10 +144,10 @@ $(document).ready(function() {
         }
     });
     // browse button function
-    $("#browseBtn").click(function(e) {
+    $("#browseBtn").click(function (e) {
         e.preventDefault();
         $("#rotate-btn").html("<div class='loader ml-5'></div>");
-        setTimeout(function() {
+        setTimeout(function () {
             window.location = "/api/code/";
             $("#rotate-btn").html("");
         }, 3000);
