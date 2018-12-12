c639731e192e93790b20baddc5e0083edead7e8d
Sean Brennan <spb4578@gmail.com>
2018-12-11 21:20:35 -0600

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(ignoreFavicon);
 app.use("/", indexRouter);
 
 const exphbs = require("express-handlebars");
@@ -38,13 +37,5 @@ db.sequelize.sync({}).then(function() {
         console.log(`Server listening on http://localhost:${PORT}`);
     });
 });
-// // this function removes the Get favicon
-// function ignoreFavicon(req, res, next) {
-//     if (req.originalUrl === "/favicon.ico") {
-//         res.status(204).json({ nope: true });
-//     } else {
-//         next();
-//     }
-// }
 
 module.exports = app;
