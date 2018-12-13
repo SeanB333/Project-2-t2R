fe7fd1f452491a3a277ee7639510d025799c0ffe
Sean Brennan <spb4578@gmail.com>
2018-12-12 23:57:05 -0600

    reset the node_env status

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-//app.set("view engine", "hbs");
+app.set("view engine", "hbs");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,9 +26,17 @@ app.engine(
         partialsDir: "views/partials"
     })
 );
-app.set("view engine", ".hbs");
 
 // error handler
+app.use(function(err, req, res) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
 
 const db = require("./models");
 const PORT = process.env.PORT || 8080;
